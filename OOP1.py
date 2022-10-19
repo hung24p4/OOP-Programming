38d1dae2093e5165dc58f121fc4c043cc341ff8c
hung24p4 <hung24p4@gmail.com>
2022-10-19 14:43:15 +0700

    OOP1

--- OOP1.py
+++ OOP1.py
@@ -1,1 +1,13 @@
-import numpy as np
+class Vampire:
+    def __init__(self, N, G, P):
+        self.name = N
+        self.gender = G
+        self.power = P
+
+    def intro(self):
+        print('My name is', self.name)
+        print('I am', self.gender)
+        print(self.power, 'is what I can do')
+
+Vampire_A = Vampire('Gabriel', 'Male', 'Shooting beam from eyes')
+Vampire_A.intro()
